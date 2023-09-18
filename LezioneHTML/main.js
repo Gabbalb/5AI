174a6e7b9b5f9b92c7e2319a7607ca30ae7d8baf
Gabriele <balbiani.gabriele@gmail.com>
2023-09-18 10:19:53 +0200

    Add function Displayname

--- LezioneHTML/main.js
+++ LezioneHTML/main.js
@@ -1,1 +1,7 @@
-var uno = Document.getElementById("")
+var nome = document.getElementById("nome").value
+var tagnome = document.getElementById("tagnome")
+alert(nome);
+function displayname(){
+    tagnome.appendChild(document.createTextNode(nome));
+    tagnome.appendChild(document.createElement("br"));
+}
